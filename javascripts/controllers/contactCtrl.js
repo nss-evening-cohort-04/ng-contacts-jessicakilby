219949e88c95ca847aff3b3228dcab9ab8a05496
jessica kilby <jessicakilby@gmail.com>
2016-11-16 09:31:04 -0600

    all contact and add new view buttons working

--- javascripts/controllers/contactCtrl.js
+++ javascripts/controllers/contactCtrl.js
@@ -2,6 +2,18 @@
 
 contact.controller("contactCtrl", function($scope){
 
+	$scope.allContactView = function(){
+		console.log("all the contacts here");
+		$scope.showListView = true;
+	};
+	$scope.addContactView = function(){
+		console.log("you want to add a contact");
+		$scope.showListView = false;
+	};
+	$scope.submitContactBtn = function(){
+		console.log("submit button for form");
+	};
+
 });
 
 // main html view, where you can see contacts, needs ng-controller="contactCtrl"
